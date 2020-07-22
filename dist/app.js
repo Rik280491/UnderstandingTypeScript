6acd521653c960fec8bc656eb9f61f9d8663e1cb
RikBala <rik280491@googlemail.com>
2020-07-22 12:59:46 +0100

    TS Compiler and Configuration

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+"use strict";
+let userInput;
+let userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+const generateError = (message, code) => {
+    throw { message: message, errorCode: code };
+};
+generateError('An error occured', 500);
+//# sourceMappingURL=app.js.map
